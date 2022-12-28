ba1df0c0749300ac08083a9a45e6e117dc3da1e1
Ganesh Futane <63470761+gatsby003@users.noreply.github.com>
2022-12-28 19:17:46 +0530

    added videoListing model

--- ytsync/models.py
+++ ytsync/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class VideoListing(models.Model):
+    """
+        Video Listing Model stores the video data.
+    """
+    video_id = models.CharField(max_length=100, null=False, unique=True)
+    title = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=100, null=False)
+    publishedAt = models.DateTimeField(null=False)
+    thumbnailUrls = models.JSONField(null=False)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['-publishedAt']
+        indexes = [
+            models.Index(fields=['title'])
+        ]
